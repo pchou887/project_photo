fa3d0c97109da1d69b41239278b654b0b61a0259
Jerry <pchou800@gmail.com>
2022-11-24 22:47:50 +0800

    fin

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div style={{ minHeight: "100vh" }}>
+      <h1>
+        <a href="../../../Project_Resume/index.html">About me</a>
+      </h1>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/pages/Homepage.js
@@ -0,0 +1,79 @@
+import React, { useState, useEffect } from "react";
+import Search from "../components/Search";
+import Picture from "../components/Picture";
+
+const Homepage = () => {
+  const [input, setInput] = useState("");
+  let [data, setData] = useState(null);
+  let [page, setPage] = useState(1);
+  let [currentSearch, setCurrentSearch] = useState("");
+  const auth = "563492ad6f91700001000001abb97b15b805407dac5445fe86466366";
+  const intialURL = "https://api.pexels.com/v1/curated?page=1&per_page=15";
+  const searchURL = `https://api.pexels.com/v1/search?query=${currentSearch}&per_page=15&page=1`;
+
+  const search = async (url) => {
+    setPage(2);
+    const dataFetch = await fetch(url, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: auth,
+      },
+    });
+    let parsedData = await dataFetch.json();
+    setData(parsedData.photos);
+  };
+
+  const morepicture = async () => {
+    let newURL;
+    if (input === "") {
+      newURL = `https://api.pexels.com/v1/curated?page=${page}&per_page=15`;
+    } else {
+      newURL = `https://api.pexels.com/v1/search?query=${currentSearch}&per_page=15&page=${page}`;
+    }
+    setPage(page + 1);
+    const dataFetch = await fetch(newURL, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: auth,
+      },
+    });
+    let parsedData = await dataFetch.json();
+    setData(data.concat(parsedData.photos));
+  };
+
+  useEffect(() => {
+    search(intialURL);
+  }, []);
+
+  useEffect(() => {
+    if (currentSearch === "") {
+      search(intialURL);
+    } else {
+      search(searchURL);
+    }
+  }, [currentSearch]);
+
+  return (
+    <div style={{ minHeight: "100vh" }}>
+      <Search
+        search={() => {
+          setCurrentSearch(input);
+        }}
+        setInput={setInput}
+      />
+      <div className="pictures">
+        {data &&
+          data.map((d) => {
+            return <Picture data={d} />;
+          })}
+      </div>
+      <div className="morePicture">
+        <button onClick={morepicture}>Load More</button>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
